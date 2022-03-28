eecfedee4804be9b58b621e40e6650cfd9424648
aadarshjha <jha.aadarsh@gmail.com>
2022-03-28 16:52:50 -0500

    pushes react

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import internshipData from './table_dict.json';
+
+function App() {
+  console.log(internshipData);
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
